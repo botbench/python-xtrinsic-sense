e3861a45d6c11c59ed13db3072b68bf3a22a80dc
Xander Soldaat <xander@botbench.com>
2014-08-16 17:58:57 +0200

    Fixed byte shifting of sensor values

--- xtrinsic_sense.py
+++ xtrinsic_sense.py
@@ -11,7 +11,6 @@ class MMA8491:
   REG_Z_LSB = 0x06
   
   def __init__(self, bus, gpio, address = 0x55, en_pin = 22):
-    print "address: 0x%02X, en_pin: %d" % (address, en_pin)
     self.bus = bus
     self.address = address
     self.en_pin = en_pin
@@ -38,11 +37,24 @@ class MMA8491:
   
     data = self.bus.read_i2c_block_data(self.address, self.REG_X_MSB, 6)
     # X axis
-    axes.append((data[0] << 8) + data[1])
+    axes.append((data[0] << 6) + (data[1] & 0x3F))
     # Y axis
-    axes.append((data[2] << 8) + data[3])
+    axes.append((data[2] << 6) + (data[3] & 0x3F))
     # Z axis
-    axes.append((data[4] << 8) + data[5])
+    axes.append((data[4] << 6) + (data[5] & 0x3F))
     
     # Set the sensor in STANDBY mode
     return axes
+
+class MAG3110:
+
+  def __init__(self, bus, gpio, address = 0x55, en_pin = 22):
+    self.bus = bus
+    self.address = address
+    self.en_pin = en_pin
+    self.gpio = gpio
+    self.gpio.setwarnings(False)
+    self.gpio.setup(self.en_pin, self.gpio.OUT)
+    self.gpio.setwarnings(True)
+    self.gpio.output(self.en_pin, False)
+    self.gpio.output(self.en_pin, True)
